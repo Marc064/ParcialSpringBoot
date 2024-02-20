d8ca628d365ac1a2ae4923dca9179d5a74513a0f
Marc064 <95501065+Marc064@users.noreply.github.com>
2024-02-20 14:55:59 -0500

    Initial commit

---
+++ src/main/java/com/edu/uptc/parcial/ParcialSpringBootMarcoVargasApplication.java
@@ -0,0 +1,13 @@
+package com.edu.uptc.parcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialSpringBootMarcoVargasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialSpringBootMarcoVargasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/uptc/parcial/ParcialSpringBootMarcoVargasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.uptc.parcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialSpringBootMarcoVargasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
